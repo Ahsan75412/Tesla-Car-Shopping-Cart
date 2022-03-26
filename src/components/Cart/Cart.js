48fbf2df1283272baea88e1dbc560c616c7ca820
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-26 21:09:09 +0600

    some issus fix here

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,18 +7,18 @@ const Cart = (props) => {
     console.log(cart);
 
     let total = 0;
-    let shipping = 0;
+
     let quantity = 0;
 
-   
+
+
+
     return (
         <div className='cart'>
-            <h4>Order Summary</h4>
+            <h4>Order Summary In Cart</h4>
             <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
-            <p>Total Shipping: ${shipping}</p>
-         
-     
+           
         </div>
     );
 };
